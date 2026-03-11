8bbfd52416c8ee1629cc9ff8ed77936931404cb8
HeTianci <a2647915766@outlook.com>
2026-03-11 20:25:44 +0800

    提交作业
    提交作业

---
+++ 第一次作业/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class primenumber
+    {
+        int m;
+        int n;
+        public primenumber(int m,int n)
+        {
+            this.m = m;
+            this.n = n;
+        }
+        public void Isprime()
+        {
+            if (m > n) 
+            {
+                int t = m;
+                m = n;
+                n = t; 
+            }
+
+            for(int i = m; i <= n; i++)
+            {
+                bool isprimed = true;
+                for(int j = 2; j <= i / 2; j++) {
+                    if (i % j == 0)
+                    {
+                        isprimed = false;
+                        break;
+                    }
+                }
+                if (isprimed&&i!=1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string m, n;
+            m=Console.ReadLine();
+            n=Console.ReadLine();
+            primenumber pn = new primenumber(int.Parse(m),int.Parse(n));
+            pn.Isprime();
+            Console.WriteLine("你好，我是张宸豪");
+        }
+    }
+}
